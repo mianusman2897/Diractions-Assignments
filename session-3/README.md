a9d1c275a9a87a77301767cd7c1e8c5633030ee2
JsTitan <sherazahmdd@gmail.com>
2023-11-28 16:20:26 +0500

    End of Session 3

--- session-3/README.md
+++ session-3/README.md
@@ -8,3 +8,7 @@ What we've covered today.
 
 Class Task 1: Build a array of objects for everyone's basic info in the class. 
 [{name:'', age: 0, email: '', phone: ''}]
+
+Home Assignment: Sort the Array of Objects with Name and Age
+
+
--- session-3/main.js
+++ session-3/main.js
@@ -1,1 +1,8 @@
-const batch = [{name:'', age: 0, email: '', phone: ''}]
+[
+    {name:'Abbas', id: 1},
+    {name:'Faizan', id: 2},
+    {name:'Usman', id: 4},
+    {name:'Moeez', id: 5},
+    {name:'Faisal', id: 3},
+    {name:'Misbah', id: 6},
+]
